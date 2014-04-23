8bcd85de807cf5c9d4e8d80075b7fc269c2dd6d6
EvanRoda <evan.roda@gmail.com>
2014-04-23 22:09:08 +0400

    Images

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ var server = http.createServer(app);
  */
 var gameDelay = 1500;  // пока полторы чтобы не часто
 var players = [];
+var game_objects = [];
 
 //Координаты сетки
 var grid_start = 135;
@@ -21,19 +22,35 @@ var grid_delta = 270;
 
 var grid = [
     {x: 135, y: 135, side: 'leaf', is_free: true, number: 0},
-    {x: 135, y: 405, side: 'leaf', is_free: true, number: 1},
+    {x: 135, y: 405, side: 'leaf', is_free: false, number: 1},
     {x: 135, y: 675, side: 'leaf', is_free: true, number: 2},
     {x: 405, y: 135, side: 'leaf', is_free: true, number: 3},
     {x: 405, y: 405, side: 'leaf', is_free: true, number: 4},
     {x: 405, y: 675, side: 'leaf', is_free: true, number: 5},
 
     {x: 135, y: 135, side: 'fire', is_free: true, number: 0},
-    {x: 135, y: 405, side: 'fire', is_free: true, number: 1},
+    {x: 135, y: 405, side: 'fire', is_free: false, number: 1},
     {x: 135, y: 675, side: 'fire', is_free: true, number: 2},
     {x: 405, y: 135, side: 'fire', is_free: true, number: 3},
     {x: 405, y: 405, side: 'fire', is_free: true, number: 4},
     {x: 405, y: 675, side: 'fire', is_free: true, number: 5}
 ];
+
+function CreateGameObject(type, x, y, parent_id){
+    var player = _.findWhere(players, {_id: parent_id});
+    if(player){
+        this.type = type;
+        this.x = x;
+        this.y = y;
+        this.parent_id = parent_id;
+        this.side = player.side;
+        if(type == 'canon'){
+            this.direction = 90;
+        }else if(type == 'hull'){
+            this.direction = 0;
+        }
+    }
+}
 
 app.use(express.static(path.join(__dirname, 'static')));
 
@@ -48,10 +65,10 @@ io.sockets.on('connection', function(socket){
         players.push(data);
     });
     socket.on('create_player_object', function(data){
-
+        game_objects.push(new CreateGameObject('canon', 405, 405, data.parent_id));
     });
 });
 
 var intId = setInterval(function(){
-    io.sockets.emit('gamedata', players);
+    io.sockets.emit('gamedata', game_objects);
 }, gameDelay);
